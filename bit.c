1221b10548e7f212a8180108ecf94af9d5f5c110
Dhruv Patel <dhruvizawesom19@gmail.com>
2025-10-02 15:32:55 -0400

    added more of the methods for bit manipulation and printing bits

--- bit.c
+++ bit.c
@@ -23,3 +23,42 @@ unsigned int ToggleBit(unsigned int value, int position){
     }
     return value ^ (1U << position);
 }
+
+int GetBit(unsigned int value, int position){
+    if(position < 0 || position >= 32){
+        return 0;
+    }
+    return !!(value & (1U << position));
+}
+
+int CountBits(unsigned int value){
+    return __builtin_popcount(value);
+}
+
+unsigned int ShiftLeft(unsigned int value, int positions){
+    if(positions < 0){
+        return ShiftRight(value, -positions);
+    }
+    if(positions >= 32){
+        return 0;
+    }
+    return value << positions;
+}
+
+unsigned int ShiftRight(unsigned int value, int positions){
+    if(positions < 0){
+        return ShiftLeft(value, -positions);
+    }
+    if(positions >= 32){
+        return 0;
+    }
+    return value >> positions;
+}
+
+void PrintBinary(unsigned int value){
+
+}
+
+void PrintHex(unsigned int value){
+    
+}
