9dfbe6c2401a211c78122e5c98771a4f1a9fd2dd
Dhruv Patel <dhruvizawesom19@gmail.com>
2025-10-03 10:43:02 -0400

    Finished board.c draft

--- board.c
+++ board.c
@@ -13,5 +13,22 @@ void InitializeBoard(GameState *game){
 }
 
 void PrintBoard(const GameState *game){
-   
+   printf("\n   A B C D E F G H\n");
+    for (int row = 7; row >= 0; row--) {
+        printf("%d  ", row + 1);
+        for (int col = 0; col < 8; col++) {
+            int pos = row * 8 + col;
+            unsigned long long mask = 1ULL << pos;
+
+            char symbol = '.';
+            if (game->red_pieces & mask) symbol = 'r';
+            if (game->black_pieces & mask) symbol = 'b';
+            if (game->red_kings & mask) symbol = 'R';
+            if (game->black_kings & mask) symbol = 'B';
+
+            printf("%c ", symbol);
+        }
+        printf("%d\n", row + 1);
+    }
+    printf("   A B C D E F G H\n\n");
 }
