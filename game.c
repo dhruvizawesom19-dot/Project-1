5bebd072810451dbf7798f54bd46734052229b60
Dhruv Patel <dhruvizawesom19@gmail.com>
2025-10-03 11:10:43 -0400

    finished draft of move piece function in game.c

--- game.c
+++ game.c
@@ -5,3 +5,35 @@ int IsOccupied(const GameState *game, int position){
     return (all >> position) & 1ULL;
 }
 
+int MovePiece(GameState *game, int from, int to){
+    unsigned long long from_pos = 1ULL << from;
+    unsigned long long to_pos = 1ULL << to;
+
+    if(IsOccupied(game, to)){
+        return 0;
+    }
+    if(game->turn == 0){
+        if(game->red_pieces & from_pos){
+            game->red_pieces &= ~from_pos;
+            game->red_pieces |= to_pos;
+        } else if(game->red_kings & from_pos){
+            game->red_kings &= ~from_pos;
+            game->red_kings |= to_pos;
+        } else {
+            return 0;
+        }
+    } else {
+        if(game->black_pieces & from_pos){
+            game->black_pieces &= ~from_pos;
+            game->black_pieces |= to_pos;
+        } else if(game->black_kings & from_pos){
+            game->black_kings &= ~from_pos;
+            game->black_kings |= to_pos;
+        } else {
+            return 0;
+        }
+    }
+    game->turn ^= 1;
+    return 1;
+}
+
